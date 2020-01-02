40239779a439d6ef36477340552fc27c582b5656
Quim Picó Mora <quim@pop-os.localdomain>
2020-01-02 21:10:28 +0100

    començament buildtree iteratiu

--- treepredict.py
+++ treepredict.py
@@ -67,7 +67,7 @@ def buildtree(part, scoref=entropy, beta=0):
     best_criteria = scoref(part)
     best_sets = ()
     best_elem = ()
-    update_best_criteria = False
+    update_best_criteria = False #En cas d'estar en un node fulla amb impuresa mínima més gran que beta
     for row in range(len(part)):
         for column in range(len(part[row])-1):
             try:
@@ -91,6 +91,34 @@ def buildtree(part, scoref=entropy, beta=0):
         else:
             return decisionnode(results=unique_counts(part))
 
+def buildtree_iter(part, scoref=entropy, beta=0):
+    if len(part)==0: return decisionnode()
+    best_criteria = scoref(part)
+    best_sets = ()
+    best_elem = ()
+    node_list = []
+    sets_list = [part]
+    while best_criteria > beta:
+        data_set=sets_list.pop(0)
+        print(best_criteria)
+        for row in range(len(data_set)):
+            for column in range(len(data_set[row])-1):
+                try:
+                    set1,set2=divideset(data_set,column,float(data_set[row][column]))
+                except ValueError:
+                    set1,set2=divideset(data_set,column,data_set[row][column]) 
+                current_criteria = max(scoref(set1),scoref(set2))
+                if best_criteria > current_criteria:
+                    best_criteria = current_criteria
+                    best_sets = (set1,set2)
+                    best_elem = (row,column)
+        print(best_sets)
+        node_list.append([data_set[best_elem[0]][best_elem[1]], best_elem[1]])
+        sets_list.append(best_sets[0])
+        sets_list.append(best_sets[1])
+    return node_list
+        
+
 def classify(obj, tree):
     dataset = read(sys.argv[1])
     dataset.append(obj)
@@ -99,11 +127,14 @@ def classify(obj, tree):
     while nodes:
         current_node=nodes.pop(0)
         set1,set2=divideset(current_node[1], current_node[0].col, current_node[0].value)
-        if current_node[0].fb is None and current_node[0].tb is None and obj in current_node[1]:
+        if current_node[0].results is not None and obj in current_node[1]:
             return current_node[1]
         else:
-            nodes.append([current_node[0].fb,set2])
-            nodes.append([current_node[0].tb,set1])
+            if current_node[0].fb is not None:
+                nodes.append([current_node[0].fb,set2])
+            if current_node[0].tb is not None:
+                nodes.append([current_node[0].tb,set1])
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.exit()
@@ -112,10 +143,11 @@ if __name__ == "__main__":
     #counts = unique_counts(dat_file)
     #gini = gini_impurity(dat_file)
     #ent = entropy(dat_file)
-    tree = buildtree(part=dat_file, beta=1)
-    printtree(tree)
-    classification=classify(['google','New Zealand','yes','23','None'], tree)
-    print(classification)
+    #tree = buildtree(part=dat_file, beta=1)
+    #printtree(tree)
+    #classification=classify(['google','New Zealand','no','22','None'], tree)
+    tree_iter=buildtree_iter(part=dat_file, beta=1.0)
+    print(tree_iter)
     
 
 
